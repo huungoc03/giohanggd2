6302c6f65d5513ceb2e9e0806d5c5c082c41b373
huungoc03 <66026497+huungoc03@users.noreply.github.com>
2024-05-08 18:51:45 +0700

    Add files via upload

---
+++ giohanggd2/giohang.js
@@ -0,0 +1,129 @@
+var currentuser; // user hiện tại, biến toàn cục
+window.onload = function () {
+	khoiTao();
+	currentuser = getCurrentUser();
+	addProductToTable(currentuser);
+}
+// tạo biến tag gồm các
+
+function giohangheader() {
+	document.write(`        
+	<div class="header group">
+        <div class="logo">
+            <a href="index.html">
+                <img src="img/logo.jpg" alt="Trang chủ Smartphone Store" title="Trang chủ Smartphone Store">
+            </a>
+        </div> <!-- End Logo -->
+            <div class="tools-member">
+                <div class="member">
+                    <a onclick="checkTaiKhoan()">
+                        <i class="fa fa-user"></i>
+                        Tài khoản
+                    </a>
+                    <div class="menuMember hide">
+                        <a href="nguoidung.html">Trang người dùng</a>
+                        <a onclick="if(window.confirm('Xác nhận đăng xuất ?')) logOut();">Đăng xuất</a>
+                    </div>
+
+                </div> <!-- End Member -->
+
+                <div class="cart">
+                    <a href="giohang.html">
+                        <i class="fa fa-shopping-cart"></i>
+                        <span>Giỏ hàng</span>
+                        <span class="cart-number"></span>
+                    </a>
+                </div> <!-- End Cart -->
+
+                <!--<div class="check-order">
+                    <a>
+                        <i class="fa fa-truck"></i>
+                        <span>Đơn hàng</span>
+                    </a>
+                </div> -->
+            </div><!-- End Tools Member -->
+        </div> <!-- End Content -->
+    </div> <!-- End Header -->`)
+}
+function addProductToTable(user) {
+	var table = document.getElementsByClassName('listSanPham')[0];
+
+	var s = `
+		<tbody>
+			<tr>
+				<th>STT</th>
+				<th>Sản phẩm</th>
+				<th>Giá</th>
+				<th>Số lượng</th>
+				<th>Thành tiền</th>
+				<th>Thời gian</th>
+			</tr>`;
+	if (!user) {
+		s += `
+			<tr>
+				<td colspan="7"> 
+					<h1 style="color:red; background-color:white; font-weight:bold; text-align:center; padding: 15px 0;">
+						Bạn chưa đăng nhập !!
+					</h1> 
+				</td>
+			</tr>
+		`;
+		table.innerHTML = s;
+		return;
+	} else if (user.products.length == 0) {
+		s += `
+			<tr>
+				<td colspan="7"> 
+					<h1 style="color:green; background-color:white; font-weight:bold; text-align:center; padding: 15px 0;">
+						Giỏ hàng trống !!
+					</h1> 
+				</td>
+			</tr>
+		`;
+		table.innerHTML = s;
+		return;
+	}
+
+	var totalPrice = 0;
+	for (var i = 0; i < user.products.length; i++) {
+		var masp = user.products[i].ma;
+		var soluongSp = user.products[i].soluong;
+		var p = timKiemTheoMa(list_products, masp);
+		var price = (p.promo.name == 'giareonline' ? p.promo.value : p.price);
+		var thoigian = new Date(user.products[i].date).toLocaleString();
+		var thanhtien = stringToNum(price) * soluongSp;
+
+		s += `
+			<tr>
+				<td>` + (i + 1) + `</td>
+				<td class="noPadding imgHide">
+					<a target="_blank" href="chitietsanpham.html?` + p.name.split(' ').join('-') + `" title="Xem chi tiết">
+						` + p.name + `
+						<img src="` + p.img + `">
+					</a>
+				</td>
+				<td class="alignRight">` + price + ` ₫</td>
+				<td class="soluong" >
+					<input size="1" onchange="capNhatSoLuongFromInput(this, '` + masp + `')" value=` + soluongSp + `>
+				</td>
+				<td class="alignRight">` + numToString(thanhtien) + ` ₫</td>
+				<td style="text-align: center" >` + thoigian + `</td>
+			</tr>
+		`;
+	}
+	s += `
+			<tr style="font-weight:bold; text-align:center">
+				<td colspan="4">TỔNG TIỀN: </td>
+				<td class="alignRight">` + numToString(totalPrice) + ` ₫</td>
+			</tr>
+		</tbody>
+	`;
+
+	table.innerHTML = s;
+}
+
+
+
+
+
+
